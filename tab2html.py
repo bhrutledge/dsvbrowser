c0b2a99c3281341b22b4bbf7aee304a37b9d3b45
Brian Rutledge <bhrutledge@gmail.com>
2013-09-26 13:31:29 -0400

    Added 404 errors, resolving #6

--- tab2html.py
+++ tab2html.py
@@ -3,7 +3,7 @@ import glob
 import os
 import sys
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 from werkzeug import secure_filename
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -46,18 +46,25 @@ def parse_input(in_path):
 
 @app.route('/')
 def list_directories():
-    directories = [ d for d in os.listdir(REPORT_DIR) 
-                    if os.path.isdir(os.path.join(REPORT_DIR, d)) ]
+    try:
+        directories = [ d for d in os.listdir(REPORT_DIR) 
+                        if os.path.isdir(os.path.join(REPORT_DIR, d)) ]
+    except OSError:
+        abort(404)
+
     return render_template('directories.html', directories=directories)
 
 
 @app.route('/<directory>', methods=['GET', 'POST'])
 def list_reports(directory):
+    if not os.path.isdir(os.path.join(REPORT_DIR, directory)):
+        abort(404)
+
     if request.method == 'POST':
-        file = request.files['file']
-        if file and file.filename.endswith(REPORT_EXT):
-            report_path = get_report_path(directory, file.filename)
-            file.save(report_path)
+        report_file = request.files['file']
+        if report_file and report_file.filename.endswith(REPORT_EXT):
+            report_path = get_report_path(directory, report_file.filename)
+            report_file.save(report_path)
 
             return redirect(url_for('show_report', directory=directory, 
                                     report=get_report(report_path)))
@@ -69,7 +76,12 @@ def list_reports(directory):
 @app.route('/<directory>/<report>')
 def show_report(directory, report):
     report_path = get_report_path(directory, report + REPORT_EXT)
-    title, head, body = parse_input(report_path)
+
+    try:
+        title, head, body = parse_input(report_path)
+    except IOError:
+        abort(404)
+        
     return render_template(directory + '.html', 
                            directory=directory, report=report,
                            title=title, head=head, body=body)
