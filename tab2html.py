a28eb0091b9df37d736408dae8b4d8ab7bd564c8
Brian Rutledge <bhrutledge@gmail.com>
2013-09-26 10:49:03 +0000

    Display report after uploaded, closes #4

--- tab2html.py
+++ tab2html.py
@@ -3,7 +3,7 @@ import glob
 import os
 import sys
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -11,6 +11,19 @@ REPORT_DIR = os.path.join(SCRIPT_DIR, 'reports')
 REPORT_EXT = '.txt'
 
 app = Flask(__name__)
+
+
+def get_report(filename):
+    return os.path.splitext(os.path.basename(filename))[0]
+
+
+def get_report_path(directory, filename):
+    return os.path.join(REPORT_DIR, directory, secure_filename(filename))
+    
+
+def get_reports(directory):
+    filenames = glob.glob(os.path.join(REPORT_DIR, directory, '*' + REPORT_EXT))
+    return [ get_report(f) for f in filenames]
 
 
 def nonblank_lines(line_iter):
@@ -43,24 +56,23 @@ def list_reports(directory):
     if request.method == 'POST':
         file = request.files['file']
         if file and file.filename.endswith(REPORT_EXT):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(REPORT_DIR, directory, filename))
+            report_path = get_report_path(directory, file.filename)
+            file.save(report_path)
 
+            return redirect(url_for('show_report', directory=directory, 
+                                    report=get_report(report_path)))
 
-    reports = glob.glob(os.path.join(REPORT_DIR, directory, '*' + REPORT_EXT))
-    reports = [ os.path.splitext(os.path.basename(f))[0] for f in reports]
-    return render_template('reports.html',
-                           directory=directory, reports=reports)
+    return render_template('reports.html', directory=directory,
+                           reports=get_reports(directory))
 
 
 @app.route('/<directory>/<report>')
 def show_report(directory, report):
-    # TODO: Test file
-    report_path = os.path.join(REPORT_DIR, directory, report + REPORT_EXT)
-    title, head_row, body_rows = parse_input(report_path)
+    report_path = get_report_path(directory, report + REPORT_EXT)
+    title, head, body = parse_input(report_path)
     return render_template(directory + '.html', 
                            directory=directory, report=report,
-                           title=title, head=head_row, body=body_rows)
+                           title=title, head=head, body=body)
 
 
 if __name__ == '__main__':
