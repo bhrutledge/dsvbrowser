ec076efccc58cb494ee81374a21e8e1908e442c5
Brian Rutledge <bhrutledge@gmail.com>
2013-10-15 07:58:37 -0400

    Moved functions into class

--- tab2html.py
+++ tab2html.py
@@ -18,43 +18,49 @@ DIRS_TEMPLATE = 'directories' + TEMPLATE_EXT
 REPORTS_TEMPLATE = 'reports' + TEMPLATE_EXT
 
 
-def get_report_name(report_fname):
-    return os.path.splitext(os.path.basename(report_fname))[0]
-
-
-def get_report_path(report_dir, report_fname):
-    return os.path.join(REPORT_DIR, report_dir, secure_filename(report_fname))
-
-
-def upload_report(report_dir, report_file):
-    if report_file and report_file.filename.endswith(REPORT_EXT):
-        report_path = get_report_path(report_dir, report_file.filename)
-        report_file.save(report_path)
-        return get_report_name(report_path)
-
-    return None
-
-
-def nonblank_lines(line_iter):
-    for line in (l.strip() for l in line_iter):
-        if line:
-            yield line
-
-
 class Report(object):
-    # TODO: Add @classmethods from_path, from_name, from_upload
 
-    def __init__(self, report_path):
-        # TODO: self.date
-        self.name = get_report_name(report_path)
+    @staticmethod
+    def _get_path(report_dir, report_fname):
+        return os.path.join(REPORT_DIR, report_dir,
+                            secure_filename(report_fname))
 
-        with open(report_path, 'U') as report_file:
-            line_iter = nonblank_lines(report_file)        
+    @staticmethod
+    def _nonblank_lines(line_iter):
+        for line in (l.strip() for l in line_iter):
+            if line:
+                yield line
+    
+    # TODO: s/name/slug
+    # TODO: s/report_//
+    def __init__(self, path, content=None):
+        self.path = path
+        self.name = os.path.splitext(os.path.basename(path))[0]
+
+        if content:
+            line_iter = self._nonblank_lines(content)        
             self.title = line_iter.next()
 
             row_iter = csv.reader(line_iter, delimiter='\t')
             self.head = row_iter.next()
             self.body = list(row_iter)
+
+    @classmethod
+    def from_path(cls, report_path):
+        with open(report_path, 'U') as report_file:
+            return cls(report_path, report_file)   
+
+    @classmethod
+    def from_name(cls, report_dir, report_name):
+        report_path = cls._get_path(report_dir, report_name + REPORT_EXT)
+        return cls.from_path(report_path)
+
+    @classmethod
+    def from_upload(cls, report_dir, report_file):
+        if report_file and report_file.filename.endswith(REPORT_EXT):
+            report_path = cls._get_path(report_dir, report_file.filename)
+            report_file.save(report_path)
+            return cls(report_path)    
 
 
 @app.route('/')
@@ -74,14 +80,14 @@ def list_reports(report_dir):
         abort(404)
 
     if request.method == 'POST':
-        report_name = upload_report(report_dir, request.files['file'])
-        # TODO: Show error message
-        if report_name:
+        # TODO: Show error messages
+        report = Report.from_upload(report_dir, request.files['file'])
+        if report:
             return redirect(url_for('show_report', report_dir=report_dir, 
-                                    report_name=report_name))
+                                    report_name=report.name))
 
     report_glob = os.path.join(REPORT_DIR, report_dir, '*' + REPORT_EXT)
-    reports = [ Report(f) for f in glob.glob(report_glob) ]
+    reports = [ Report.from_path(f) for f in glob.glob(report_glob) ]
 
     return render_template(REPORTS_TEMPLATE,
                            report_dir=report_dir, reports=reports)
@@ -89,10 +95,8 @@ def list_reports(report_dir):
 
 @app.route('/<report_dir>/<report_name>')
 def show_report(report_dir, report_name):
-    report_path = get_report_path(report_dir, report_name + REPORT_EXT)
-
     try:
-        report = Report(report_path)
+        report = Report.from_name(report_dir, report_name)
     except IOError:
         abort(404)
 
