adae415fba097b3c5ae2995bd6cfcfe9556d48d9
Brian Rutledge <bhrutledge@gmail.com>
2013-11-01 10:16:26 -0400

    Added action parameter

--- tab2html/views.py
+++ tab2html/views.py
@@ -7,12 +7,10 @@ from .utils import *
 
 frontend = Blueprint('frontend', __name__)
 
-
 @frontend.route('/')
 def list_subdirs():
     subdirs = os.walk(report_dir_path()).next()[1]
     return render_template(DIRS_TEMPLATE, subdirs=subdirs)
-
 
 @frontend.route('/<subdir>', methods=['GET', 'POST'])
 def list_reports(subdir):
@@ -20,17 +18,21 @@ def list_reports(subdir):
     report_dir = ReportDirectory(report_dir_path(subdir))
 
     if request.method == 'POST':
-        upload = request.files['file']
-        if valid_upload(upload):
-            try:
-                report = report_dir.upload_file(upload)
-            except EnvironmentError as e:
-                raise_errno(e)
+        action = request.args.get('action', '')
 
-            return redirect(
-                url_for('.show_report', subdir=subdir, slug=report.slug))
-        else:
-            error = "Invalid file"
+        # TODO: Look into WTForms
+        if action == 'upload':
+            upload = request.files['file']
+            if valid_upload(upload):
+                try:
+                    report = report_dir.upload_file(upload)
+                except EnvironmentError as e:
+                    raise_errno(e)
+
+                return redirect(
+                    url_for('.show_report', subdir=subdir, slug=report.slug))
+            else:
+                error = "Invalid file"
 
     try:
         reports = report_dir.get_reports()
@@ -50,6 +52,7 @@ def show_report(subdir, slug):
         raise_errno(e)
     
     return render_template(subdir + TEMPLATE_EXT, subdir=subdir, report=report)
+
 
 @frontend.route('/<subdir>/<slug>/delete', methods=['POST'])
 def delete_report(subdir, slug):
--- tests/test_views.py
+++ tests/test_views.py
@@ -4,6 +4,11 @@ from tab2html.constants import *
 from .utils import AppTestCase
 
 
+SUBDIR = 'inventory'
+SUBDIR_SLUGS = {'inventory' : ['report_one', 'report_two']}
+UPLOAD_ACTION = '?action=upload'
+
+
 def view_url(*args):
     return '/'.join([''] + list(args))
 
@@ -12,24 +17,21 @@ class ViewTestCase(AppTestCase):
 
     def setUp(self):
         super(ViewTestCase, self).setUp()
-
         self.client = self.app.test_client()
-        self.subdir = 'inventory'
-        self.subdir_slugs = {'inventory' : ['report_one', 'report_two']}
 
     def test_list_subdirs(self):
         rsp = self.client.get(view_url(''))
         self.assertEqual(rsp.status_code, 200)
-        for subdir in self.subdir_slugs:
+        for subdir in SUBDIR_SLUGS:
             self.assertIn(subdir, rsp.data)
 
     def test_list_reports(self):
-        for subdir in self.subdir_slugs:
+        for subdir in SUBDIR_SLUGS:
             rsp = self.client.get(view_url(subdir))
             self.assertEqual(rsp.status_code, 200)
             self.assertIn(subdir, rsp.data)
             
-            for slug in self.subdir_slugs[subdir]:
+            for slug in SUBDIR_SLUGS[subdir]:
                 self.assertIn(view_url(subdir, slug), rsp.data)
 
     def test_list_reports_not_found(self):
@@ -40,14 +42,14 @@ class ViewTestCase(AppTestCase):
         pass
 
     def test_show_report(self):
-        for subdir in self.subdir_slugs:
-            for slug in self.subdir_slugs[subdir]:
+        for subdir in SUBDIR_SLUGS:
+            for slug in SUBDIR_SLUGS[subdir]:
                 rsp = self.client.get(view_url(subdir, slug))
                 self.assertEqual(rsp.status_code, 200)
                 self.assertIn(slug, rsp.data)
 
     def test_show_report_not_found(self):
-        for subdir in self.subdir_slugs:
+        for subdir in SUBDIR_SLUGS:
             rsp = self.client.get(view_url(subdir, 'foo'))
             self.assertEqual(rsp.status_code, 404)
 
@@ -59,11 +61,11 @@ class ViewTestCase(AppTestCase):
     def test_upload_file(self):
         slug = 'test'
 
-        rsp = self.client.post(view_url(self.subdir), 
+        rsp = self.client.post(view_url(SUBDIR + UPLOAD_ACTION), 
             data={'file': (StringIO('foo\nbar'), slug + REPORT_EXT)})
 
         self.assertEqual(rsp.status_code, 302)
-        self.assertIn(view_url(self.subdir, slug), rsp.location)
+        self.assertIn(view_url(SUBDIR, slug), rsp.location)
 
     def test_upload_file_redirect(self):
         slug = 'test'
@@ -74,7 +76,7 @@ class ViewTestCase(AppTestCase):
         data one\tdata two\t
         """
 
-        rsp = self.client.post(view_url(self.subdir), 
+        rsp = self.client.post(view_url(SUBDIR) + UPLOAD_ACTION, 
             data={'file': (StringIO(contents), slug + REPORT_EXT)},
             follow_redirects=True)
 
@@ -84,7 +86,7 @@ class ViewTestCase(AppTestCase):
         self.assertIn('data two', rsp.data)
 
     def test_upload_file_invalid(self):
-        subdir_url = view_url(self.subdir)
+        subdir_url = view_url(SUBDIR)
         invalid_data = [
             {'file': (StringIO(''), '')},
             {'file': (StringIO('foo\nbar'), '')},
@@ -93,43 +95,43 @@ class ViewTestCase(AppTestCase):
         ]
 
         for data in invalid_data:
-            rsp = self.client.post(subdir_url, data=data)
+            rsp = self.client.post(subdir_url + UPLOAD_ACTION, data=data)
             self.assertEqual(rsp.status_code, 200)
             self.assertIn('Invalid file', rsp.data)
-            self.assertIn(self.subdir, rsp.data)
+            self.assertIn(SUBDIR, rsp.data)
 
     def test_upload_file_not_found(self):
-        rsp = self.client.post(view_url('foo'), 
+        rsp = self.client.post(view_url('foo') + UPLOAD_ACTION, 
             data={'file': (StringIO('foo\nbar'), 'foo.txt')},
             follow_redirects=True)
 
         self.assertEquals(rsp.status_code, 404)
 
     def test_delete_report(self):
-        slug = self.subdir_slugs[self.subdir][0]
+        slug = SUBDIR_SLUGS[SUBDIR][0]
 
-        rsp = self.client.post(view_url(self.subdir, slug, 'delete'))
+        rsp = self.client.post(view_url(SUBDIR, slug, 'delete'))
         self.assertEqual(rsp.status_code, 302)
-        self.assertIn(view_url(self.subdir), rsp.location)
+        self.assertIn(view_url(SUBDIR), rsp.location)
 
     def test_delete_report_redirect(self):
-        slug = self.subdir_slugs[self.subdir][0]
+        slug = SUBDIR_SLUGS[SUBDIR][0]
 
-        rsp = self.client.post(view_url(self.subdir, slug, 'delete'),
+        rsp = self.client.post(view_url(SUBDIR, slug, 'delete'),
                                follow_redirects=True)
 
         self.assertEqual(rsp.status_code, 200)
         self.assertNotIn(slug, rsp.data)
 
     def test_delete_report_get(self):
-        slug = self.subdir_slugs[self.subdir][0]
+        slug = SUBDIR_SLUGS[SUBDIR][0]
 
-        rsp = self.client.get(view_url(self.subdir, slug, 'delete'))
+        rsp = self.client.get(view_url(SUBDIR, slug, 'delete'))
         self.assertEqual(rsp.status_code, 405)
 
     def test_delete_report_not_found(self):
         slug = 'foo'
-        rsp = self.client.post(view_url(self.subdir, slug, 'delete'),
+        rsp = self.client.post(view_url(SUBDIR, slug, 'delete'),
                               follow_redirects=True)
         
         self.assertEqual(rsp.status_code, 404)
